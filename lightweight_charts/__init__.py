b01df74707f7c9801b4665b8e2a4e5ab3bd71b7a
David Brazda <davidbrazda61@gmail.com>
2024-06-14 10:56:22 +0200

    fix init

--- lightweight_charts/__init__.py
+++ lightweight_charts/__init__.py
@@ -2,4 +2,4 @@ from .abstract import AbstractChart, Window
 from .chart import Chart
 from .widgets import JupyterChart
 from .polygon import PolygonChart
-from .helpers import chart, Panel
+from .helpers import chart, Panel, PlotAccessor
